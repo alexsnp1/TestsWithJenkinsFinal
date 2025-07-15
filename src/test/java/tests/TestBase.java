0b894abdc64920399a3f67846f2be22e6b8c95ce
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-07-15 11:21:52 +0700

    added remote connection

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -10,6 +12,8 @@ public class TestBase {
         Configuration.browserSize = "1600x900";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
